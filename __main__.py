aa3bd1b67bc55477b85f3a7a6cb0775ca3e9068e
Jerome <jerome.siljan@mavs.uta.edu>
2022-10-16 20:19:31 -0500

    feat(__main__.py): Uses CRAFT to get polys of words

--- __main__.py
+++ __main__.py
@@ -1,17 +1,62 @@
-# import Craft class
-from craft_text_detector import Craft
+import matplotlib.pyplot as plt
+import keras_ocr
+
+# import craft functions
+from craft_text_detector import (
+    read_image,
+    load_craftnet_model,
+    load_refinenet_model,
+    get_prediction,
+    export_detected_regions,
+    export_extra_results,
+    empty_cuda_cache,
+)
 
 # set image path and export folder directory
-image = 'figures/idcard.png' # can be filepath, PIL image or numpy array
-output_dir = 'outputs/'
+image_path = "test1.jpeg"  
+output_dir = "outputs/"
 
-# create a craft instance
-craft = Craft(output_dir=output_dir, crop_type="poly", cuda=False)
+# read image into cv2
+image = read_image(image_path)  
 
-# apply craft text detection and export detected regions to output directory
-prediction_result = craft.detect_text(image)
+# load models
+refine_net = load_refinenet_model(
+    cuda=False, weight_path="models/craft_refiner_CTW1500.pth"
+)
+craft_net = load_craftnet_model(cuda=False, weight_path="models/craft_mlt_25k.pth")
 
-# unload models from ram/gpu
-craft.unload_craftnet_model()
-craft.unload_refinenet_model()
+# perform prediction
+prediction_result = get_prediction(
+    image=image,
+    craft_net=craft_net,
+    refine_net=refine_net,
+    text_threshold=0.7,
+    link_threshold=0.4,
+    low_text=0.4,
+    cuda=False,
+    long_size=1280,
+)
 
+poly = prediction_result["boxes"]
+# Last box
+# A B 159 1424
+# C B 934 1412
+# C D 934 1449
+# A D 160 1461
+print(poly)
+
+# # export detected text regions
+# exported_file_paths = export_detected_regions(
+#     image=image, regions=prediction_result["boxes"], output_dir=output_dir, rectify=True
+# )
+# 
+# # export heatmap, detection points, box visualization
+# export_extra_results(
+#     image=image,
+#     regions=prediction_result["boxes"],
+#     heatmaps=prediction_result["heatmaps"],
+#     output_dir=output_dir,
+# )
+
+# unload models from gpu
+empty_cuda_cache()
