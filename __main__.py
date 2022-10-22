8317ebc5aa815360aab6f267741d22c5fabe29c2
Jerome <jerome.siljan@mavs.uta.edu>
2022-10-22 08:39:37 -0500

    feat(__main__.py): clusters

--- __main__.py
+++ __main__.py
@@ -2,6 +2,7 @@ import matplotlib.pyplot as plt
 import keras_ocr
 import cv2
 import math
+from sklearn.cluster import MeanShift
 
 
 class _coords:
@@ -11,10 +12,16 @@ class _coords:
         self.bottom_right = (int(coords[2][0]), int(coords[2][1]))
         self.bottom_left = (int(coords[3][0]), int(coords[3][1]))
         self.center = (
-            int((coords[0][0] + coords[1][0] + coords[2][0] + coords[3][0]) / 4),
-            int((coords[0][1] + coords[1][1] + coords[2][1] + coords[3][1]) / 4),
+            abs(self.top_left[0] - self.bottom_right[0]) / 2,
+            abs(self.top_left[1] - self.bottom_right[1]) / 2,
         )
         self.group = 0
+        self.coords = [
+            self.top_left,
+            self.top_right,
+            self.bottom_left,
+            self.bottom_right,
+        ]
 
     def report(self):
         print(f"{self.top_left}\t{self.top_right}")
@@ -72,7 +79,55 @@ prediction_result = get_prediction(
 
 polys = [_coords(coords) for coords in prediction_result["boxes"]]
 
-image = draw_boxes(image)
+
+def color_clusters(arr, name):
+    plt.cla()
+    ms = MeanShift()
+    ms.fit(arr)
+    labels = ms.labels_
+    img = plt.imread("test1.jpeg")
+    plt.imshow(img)
+    colors = ["red", "blue", "green", "yellow"]
+    for cluster_num, point in zip(labels, arr):
+        try:
+            plt.scatter(*(point), c=colors[cluster_num])
+        except:
+            print(f"{point}, {cluster_num}")
+
+    plt.savefig(f"{output_dir}{name}.png")
+    # plt.show()
+
+
+color_clusters([poly.top_left for poly in polys], "top_left")
+color_clusters([poly.top_right for poly in polys], "top_right")
+color_clusters([poly.bottom_left for poly in polys], "bottom_left")
+color_clusters([poly.bottom_right for poly in polys], "bottom_right")
+color_clusters([poly.center for poly in polys], "center")
+
+"""
+node = polys[0].top_right
+stop = 0
+for main_poly in polys:
+    dists = []
+    nodes = []
+    for main_poly_node in main_poly.coords:
+        for poly in polys:
+            for poly_node in poly.coords:
+                if main_poly_node != poly_node:
+                    dist = math.dist(poly_node, main_poly_node)
+                    print(dist)
+                    dists.append(dist)
+                    nodes.append(poly_node)
+                    if dist == 0:
+                        stop = 1
+        min_dist = min(dists)
+        index = dists.index(min_dist)
+        node = nodes[index] 
+        print(f'min dist is {min_dist} between {node} and {main_poly_node}')
+        image = cv2.line(image, node, main_poly_node, (255, 0, 0), 2)
+        break
+print(f"stop {stop}")
+"""
 
 cv2.imshow("image", image)
 cv2.waitKey(0)
